063880bba4b39b6a36983f46d9f6f1a1c40e54ad
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-03-23 14:34:59 +0430

    change struct Analyze file

--- src/Analyze.py
+++ src/Analyze.py
@@ -3,60 +3,8 @@ import pandas
 import openpyxl
 import pandas as pd
 from openpyxl import Workbook
-	
-# Class Block
-class Block:
-	def __init__(self, name):
-		self.name = name
-		self.allocation = 0
-		self.expend = 0 
-		self.list_record = []
+from Class_Block import Block	
 
-	def print(self):
-		print("Name:" + self.name +
-		"\t allo:" +  str(self.allocation) +
-		"\t expend:" + str(self.expend) +
-		"\t remaining:" + str(self.get_remaining()))
-
-		print("list records:")
-		for record in self.list_record:	
-			print("comment:" + record['comment'] +
-			"\t val:" +  str(record['val']) +
-			"\t date:" + record['date'])
-
-	def add_expend(self, val):
-		self.expend += val 
-
-	def add_allocation(self, val):
-		self.allocation += val 
-
-	def get_remaining(self):
-		return self.allocation - self.expend
-	
-	def add_record(self, comment, val, date):
-		self.list_record.append({"comment" : comment,
-								 "val" : val,
-								 "date" : date})
-
-	def sort_record(self, type = "val", reverse = False):
-		def key_by_val(dict):
-			return dict['val']
-
-		def key_by_date(dict):
-			return dict['date']
-
-		if(type == "val"):
-			if(reverse):
-				self.list_record.sort(key=key_by_val, reverse = True)
-			else:
-				self.list_record.sort(key=key_by_val)
-	
-		if(type == "date"):
-			if(reverse):
-				self.list_record.sort(key=key_by_date, reverse = True)
-			else:
-				self.list_record.sort(key=key_by_date)
-	
 # Initialize variable from Excel
 Excel_file = '../data/System_Money.xlsx'
 Block_Data = pandas.read_excel(Excel_file, sheet_name = 'blocks')
